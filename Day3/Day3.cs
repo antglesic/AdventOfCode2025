fb4809ea724483608ef3f2fadb7671b7a8a7b8d9
Antonio Glesic <antonio.glesic@king-ict.eu>
2025-12-03 13:06:01 +0100

    Part 1 of the solution for Day3 is done Still needs part two to be done

---
+++ Day3/Day3.cs
@@ -0,0 +1,85 @@
+var banks = File.ReadAllLines("Day3PuzzleInput.txt")
+	.Select(line =>
+	{
+		string bank = line;
+		return bank;
+	})
+	.ToList();
+
+var joltSum = GetJolts(banks)
+	.Select(jolt =>
+	{
+		int power = int.Parse(jolt);
+		return power;
+	})
+	.Sum();
+//var jolts = GetJolts(new List<string>() { "5313313222133223232222173222221322232243522222432422223223232222322322332122525322132232325523243253" });
+
+Console.WriteLine("Jolt sum: " + joltSum);
+
+static List<string> GetJolts(List<string> banks)
+{
+	List<string> retval = [];
+
+	foreach (var bank in banks)
+	{
+		// 5313313222133223232222173222221322232243522222432422223223232222322322332122525322132232325523243253
+		// length = 100
+		int length = bank.Length;
+		List<int> numbers = [];
+
+		int max = 0;
+		int maxPosition = 0;
+
+		int nextMax = 0;
+		int nextMaxPosition = 0;
+
+		//Console.WriteLine("Bank: " + bank);
+		//Console.WriteLine("Length: " + length);
+
+		for (int i = 0; i < length; i++)
+		{
+			// 5
+			// 7
+			numbers.Add(int.Parse(bank[i].ToString()));
+
+			//Console.WriteLine("Char: " + bank[i].ToString());
+			//Console.WriteLine("Old Max: " + max);
+			// 5 > 0 && 1 < 100
+			// 7 > 5 && 23 < 100
+			if (numbers[i] > max && (i + 1 < length))
+			{
+				// max = 5
+				// maxPosition = 0
+
+				// max = 7
+				// maxPosition = 22
+				max = numbers[i];
+				maxPosition = i;
+			}
+
+			//Console.WriteLine("New Max: " + max);
+		}
+
+		for (int i = maxPosition + 1; i < length; i++)
+		{
+			//Console.WriteLine("Char: " + bank[i].ToString());
+			//Console.WriteLine("Old NextMax: " + nextMax);
+
+			if (numbers[i] > nextMax)
+			{
+				nextMax = numbers[i];
+				nextMaxPosition = i;
+			}
+
+			//Console.WriteLine("New NextMax: " + nextMax);
+		}
+
+		if (max > 0 && nextMax > 0)
+			retval.Add(string.Format("{0}{1}", max, nextMax));
+
+		//Console.WriteLine("Retval: " + retval);
+	}
+
+	return retval;
+}
